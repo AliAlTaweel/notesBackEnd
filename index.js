5017838a0f5e4db6717e73debbfffcb4c8e175dc
AliAlTaweel <engalihaitham@gmail.com>
2024-10-23 20:19:54 +0300

    port 10000

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.post("/api/notes", (req, res) => {
 //========================================
 
 // ============= PORT & listen ============
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
